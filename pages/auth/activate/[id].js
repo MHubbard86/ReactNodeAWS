97d6614ea183f92f15c034777bd63f356fe3b79c
Matthew Hubbard <50264772+MHubbard86@users.noreply.github.com>
2023-03-01 19:46:38 +0000

    third commit

--- pages/auth/activate/[id].js
+++ pages/auth/activate/[id].js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import jwt from 'jsonwebtoken';
 import axios from 'axios';
 import { showSuccessMessage, showErrorMessage } from '../../../helpers/alerts';
